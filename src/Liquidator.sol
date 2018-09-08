c553690afa7369ad45727692628facc9c0b2ebdb
David Terry <me@xwvvvvwx.com>
2018-09-08 10:13:03 +0200

    initial commit

--- src/Liquidator.sol
+++ src/Liquidator.sol
@@ -1,4 +1,15 @@
 pragma solidity ^0.4.24;
 
+import 'sai/tub.sol';
+import 'maker-otc/matching_market.sol';
+
 contract Liquidator {
+
+    MatchingMarket oasis;
+    SaiTub tub;
+
+    constructor(SaiTub _tub, MatchingMarket _oasis) public {
+        tub = _tub;
+        oasis = _oasis;
+    }
 }
