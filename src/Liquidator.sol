b670a485ad6f0f2b996b58bfdb4f3cc01775b6c7
David Terry <me@xwvvvvwx.com>
2018-09-08 18:49:36 +0200

    add events

--- src/Liquidator.sol
+++ src/Liquidator.sol
@@ -15,24 +15,34 @@ contract Liquidator is DSMath {
         tub = _tub;
     }
 
+    event Owner();
+    event RetrievedData(uint collateral, uint debt, uint price);
+    event Calucated(uint totalValue, uint fee, uint surplus);
+    event Shut();
+    event Transfered();
+
     function close(bytes32 _cup) public {
         // can only operate on cdps that it controls
         assert(tub.lad(_cup) == address(this));
+        emit Owner();
 
         // calcuate surplus collateral
         uint collateral = tub.ink(_cup);
         uint debt = tub.tab(_cup);
         uint price = tub.tag();
+        emit RetrievedData(collateral, debt, price);
 
         uint totalValue = wmul(collateral, price);
         uint fee = wmul(totalValue, cut);
-
         uint surplus = sub(sub(totalValue, fee), debt);
+        emit Calucated(totalValue, fee, surplus);
 
         // close the cdp
         tub.shut(_cup);
+        emit Shut();
 
         // transfer the surplus
         tub.skr().transfer(msg.sender, surplus);
+        emit Transfered();
     }
 }
